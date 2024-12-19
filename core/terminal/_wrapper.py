9d9884eda266c385c7dd30920910c61921dc8cc4
Saeid Zain Aldeen <saeid.zain.aldeen@gmail.com>
2024-12-19 14:36:21 +0300

    Add a terminal wrapper abstract class to prepare for cross-platfrom terminal support

---
+++ core/terminal/_wrapper.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class KeyboardKey(Enum):
+    # Arrow Keys
+    UP = 'UP'
+    DOWN = 'DOWN'
+    LEFT = 'LEFT'
+    RIGHT = 'RIGHT'
+
+    # TODO: add more keys
+
+
+class TerminalSize():
+    def __init__(self, rows: int, columns: int):
+        self.rows = rows
+        self.columns = columns
+        self.size = rows * columns
+
+
+class TerminalWrapperBase(ABC):
+    @abstractmethod
+    def size(self) -> TerminalSize:
+        pass
+
+    @abstractmethod
+    def write(self, row: int, col: int, text: str):
+        pass
+
+    @abstractmethod
+    def clear(self, fill: str = ' '):
+        pass
+
+    @abstractmethod
+    def read(self) -> KeyboardKey | None:
+        pass
